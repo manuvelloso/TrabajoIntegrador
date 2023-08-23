0934b609cfbec2592b54f46b975241e19ae4d63a
Manuela Velloso <65735915+manuvelloso@users.noreply.github.com>
2023-08-23 13:42:00 -0300

    Add files via upload

---
+++ EcuacionesDiferenciales/EDIF_3.m
@@ -0,0 +1,25 @@
+%% Ecuación diferencial ej#3 TP integrador 2022
+
+function V0p = EDIF_3(T,V0)
+
+% Variables del circuito T
+c = 10e-6;
+R1 = 100;
+R2 = 10e3;
+
+% Componentes de la matriz A
+a0 = 1/(c^2*R1*R2);
+a1 = (2*R1 + R2)/(c*R1*R2);
+
+% Matrices A y B
+A = [0 1; -a0 -a1];
+B = [0 1]';
+
+% Tensión de entrada 
+vi = 50*sin(100*T + pi);
+
+% Sistema de ecuaciones diferenciales
+V0p = A*V0' + B*vi;
+V0p = V0p';
+
+end
---
+++ EcuacionesDiferenciales/EDIF_4b.m
@@ -0,0 +1,31 @@
+%% Ecuación diferencial ej#4b TP integrador 2022
+
+function V0p = EDIF_4b(T,V0)
+
+% Variables del circuito T
+c = 10e-6;
+R1 = 100;
+R2 = 10e3;
+
+% Componentes de la matriz A = [a00 a01; a10 a11]
+a00 = -(R1+R2)/(c*R1*R2);
+a11 = -1/(c*R2);
+a01 = 1/(c*R2);
+a10 = a01;
+
+%Componentes de la matriz B
+b0 = 1/(c*R1);
+b1 = 0;
+
+% Matrices A y B
+A = [a00 a01; a10 a11];
+B = [b0 b1]';
+
+% Tensión de entrada
+vi = 1;
+
+% Sistema de ecuaciones diferenciales
+V0p = A*V0' + B*vi;
+V0p = V0p';
+
+end
